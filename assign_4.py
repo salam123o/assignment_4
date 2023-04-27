a7edc943c1c9ea7a5390228ccde3583680804afa
salam123o <110718122+salam123o@users.noreply.github.com>
2023-04-27 19:10:52 +0530

    added python file

---
+++ assign_4.py
@@ -0,0 +1,60 @@
+friend_names="santhosh","abhinay","ameer","nayeem","siraj","saleem","sadhik","shiva","gowtham","bhargav"
+print(friend_names)
+
+family_names="anwar","fathima","kalam","reshma","rowfa","areef","mobeen","mateen","amanu","ameen"
+print(family_names)
+
+fav_food="chicken biryani","mutton biryani","roast biryani","pizza","gongura biryani","shawrma","burger","veg manchuria","chicken 65","fish biryani"
+print(fav_food)
+
+colour_names="red","green","purple","yellow","white","black","blue","maroon","violet","sky blue"
+print(colour_names)
+
+month_names="january","february","march","april","may","june","july","august","september","october","november","december"
+print(month_names)
+
+game_name="volleyball","cricket","football","godimbilla","chess","carrom board","kabadi","hoki","shetel","free fire"
+print(game_name)
+
+city_names="machilipatnam","vijaywada","hyderabad","mumbai","bangalor","delhi","surat","goa","vizag","ahemadabad"
+print(city_names)
+
+friend__names=["santhosh","abhinay","ameer","nayeem","siraj","saleem","sadhik","shiva","gowtham","bhargav"]
+print(friend__names)
+
+family_names_tup=("anwar","fathima","kalam","reshma","rowfa","areef","mobeen","mateen","amanu","ameen")
+print(family_names_tup)
+
+colours_tup={"red","green","purple","yellow","white","black","blue","maroon","violet","sky blue"}
+print(colours_tup)
+
+set_sort={1,9,5,7,2,3,10,4,6,8}
+print(set_sort)
+
+chiru="chiranjeevi"
+ntr="n.t.ramarao"
+superstar="maheshbabu"
+powerstar="pawan kalyan"
+
+print("tollywood_heros",chiru,ntr,superstar,powerstar)
+
+
+bollywood_heros={"sharuk","salman","amita bachan","salman","akshay","sharuk"}
+print(bollywood_heros)
+
+breakfast_items=["maysoor baji","puri","egg attu","menap attu","parota","chapati"]
+
+print(breakfast_items )
+breakfast_items [4]
+
+
+
+
+
+
+
+
+
+
+
+
